dbc362b602655e77c5279b7d1ee8deeda21fb09b
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-12-05 22:16:12 -0300

    style: fix files formatting

--- test/test_ml_model.py
+++ test/test_ml_model.py
@@ -6,11 +6,11 @@ from src.core.ml.pre_processor import PreProcessor
 
 def test_model_accuracy():
     loader = DataLoader("data/dataset_test.csv")
-    dataset = loader.load(["age", "sex", "cp", "trestbps", "chol", "fbs", "num"])
+    dataset = loader.load(
+        ["age", "sex", "cp", "trestbps", "chol", "fbs", "num"])
     pre_processor = PreProcessor(dataset, 0.2)
     train_result, test_result = pre_processor.process()
     model = ModelFactory.create_model()
     model.train(train_result)
     evaluator = AccuracyEvaluator(model)
     assert evaluator.evaluate(test_result) >= 0.70
-
