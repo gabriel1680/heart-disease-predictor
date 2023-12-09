cd22564cac79e695a32739794eed0ef15fbfe35e
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-12-09 17:21:41 -0300

    test: add test documentation

--- api/test/test_ml_model.py
+++ api/test/test_ml_model.py
@@ -5,12 +5,16 @@ from src.core.ml.pre_processor import PreProcessor
 
 
 def test_model_accuracy():
+    # carregamento dos dados
     loader = DataLoader("data/dataset_test.csv")
     dataset = loader.load(
         ["age", "sex", "cp", "trestbps", "chol", "fbs", "num"])
+    # pré-processamento dos dados
     pre_processor = PreProcessor(dataset, 0.2)
     train_result, test_result = pre_processor.process()
+    # treinamento do modelo
     model = ModelFactory.create_model()
     model.train(train_result)
+    # avaliação de acurácia
     evaluator = AccuracyEvaluator(model)
     assert evaluator.evaluate(test_result) >= 0.80
